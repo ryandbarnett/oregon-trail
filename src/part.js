9db87ac9a8526b1115f4012af8645893795ed296
RyanDBarnett <conner2468@gmail.com>
2020-05-15 10:08:40 -0600

    Completed Part class

--- src/part.js
+++ src/part.js
@@ -1,5 +1,26 @@
 class Part {
+  constructor(type) {
+    this.validTypes = [
+      'wheel', 
+      'yoke', 
+      'axle', 
+      'ox'
+    ];
+    this.type = this.validTypes.find(validType => validType === type);
+    this.broken = false;
+    this.hasBeenRepaired = false;
+  }
 
+  break() {
+    this.broken = true;
+  }
+
+  repair() {
+    if (!this.hasBeenRepaired) {
+      this.broken = false;
+      this.hasBeenRepaired = true; 
+    }
+  }
 }
 
 module.exports = Part;
