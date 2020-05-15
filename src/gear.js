8ad2b5bceeffa9c71933d9ec01b178f09abb6ea4
RyanDBarnett <conner2468@gmail.com>
2020-05-15 10:02:10 -0600

    Completed Gear class

--- src/gear.js
+++ src/gear.js
@@ -1,5 +1,13 @@
 class Gear {
-
+  constructor(type, quantity) {
+    this.validTypes = [
+      'ammunition', 
+      'food', 
+      'clothes'
+    ];
+    this.type = this.validTypes.find(validType => validType === type);
+    this.quantity = quantity;
+  }
 }
 
 module.exports = Gear;
